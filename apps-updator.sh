4d439d608bc084f9c4eb38468b0b5e1e0443b6be
Rahul Kauraiya <rkauraiya1167@gmail.com>
2021-08-15 08:18:29 +0530

    apps-updator added

---
+++ apps-updator.sh
@@ -0,0 +1,12 @@
+# Step - 1
+sudo apt update
+
+# Step - 2 : To see the updates
+sudo apt list --upgradable
+
+# Step - 3 : To update
+sudo apt upgrade
+
+# Step - 4 : Cleaning system
+sudo apt autoremove
+
